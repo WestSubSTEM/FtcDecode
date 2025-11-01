850334de5172cc00b71d7a05bdcfadb930cad1cd
John Groppe <jgroppe@westsubstem.org>
2025-10-31 23:13:03 -0500

    Initial meet 1 teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/STEMperFiConstants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class STEMperFiConstants {
+    public static final double FLIPPER_INTAKE = 0.5;
+    public static final double FLIPPER_SHOOT = .25;
+
+    public static final double INDEX_1 = 0.82;
+    public static final double INDEX_2 = 0.62;
+    public static final double INDEX_3 = 0.42;
+
+    public static final long INTAKE_DURING_INDEX_MOVE_MS = 1500;
+}
